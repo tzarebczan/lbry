d0bc4107b18528cac894d8217e373afd4398f571
Jack Robison <jackrobison@lbry.io>
2017-01-08 15:19:16 -0500

    local build script

--- travis_macos.sh
+++ travis_macos.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 export ON_TRAVIS=false
-export SKIP_DMG=true
-export SKIP_SIGN=true
+export SKIP_DMG=false
+export SKIP_SIGN=false
 export SKIP_PYLINT=true
 
 ./packaging/scripts/travis/install_deps_macos.sh
